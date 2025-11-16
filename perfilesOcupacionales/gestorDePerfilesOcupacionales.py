d4ee79210ccb0572bfa63cd59e7e9318d300cfc3
SergioAndresG <140452759+SergioAndresG@users.noreply.github.com>
2025-11-15 21:23:31 -0500

    feat(gestorDePerfilesOcupacionales.py): Añade función para extraer nombre de ficha de caracterización

--- perfilesOcupacionales/gestorDePerfilesOcupacionales.py
+++ perfilesOcupacionales/gestorDePerfilesOcupacionales.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import pandas as pd
 
 # Definimos la función que va decodificar los datos de el archivo json
 def cargar_mapeo_perfiles(ruta_json='perfiles_ocupacionales.json'):
@@ -18,3 +19,18 @@ def cargar_mapeo_perfiles(ruta_json='perfiles_ocupacionales.json'):
         # Si hay un error mas profundo retornamos el error exacto
         logging.error(f"Error al lerr el json {ruta_json}")
         return {}
+    
+def extraer_nombre_ficha(nombre_ficha):
+    """
+    Extrae el nombre de la "ficha de caracterizacion"
+    Ejemplo => "3150717 - GESTION DEL TALENTO HUMANO" -> "GESTION DEL TALENTO HUMANO"
+    """
+    if not nombre_ficha or pd.isna(nombre_ficha):
+        return None
+    
+    #Dividir por el guion y tomar segunda parte
+    partes = str(nombre_ficha).split("-", 1)
+    if len(partes) > 1:
+        return partes[1].strip().upper()
+    return str(nombre_ficha).strip().upper()
+
